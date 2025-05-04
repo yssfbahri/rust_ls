748c30066409dbce378244648fcad9bacce43dae
yssfbhr <youssefbahri2001@gmail.com>
2025-05-04 03:40:18 +0100

    added basic argument parsing

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,50 @@ use std::process;
 mod ls_utils;
 use crate::ls_utils::Options;
 
-const  CONFIG : Options = Options{
-    show_hidden : false,
-    long_format : false,
-};
+use clap::Parser;
 
-fn main() {
-    let path = Path::new(".");
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// do not ignore entries starting with '.'
+    #[arg(short,long,default_value_t=false)]
+    all: bool,
+    /// use a long listing format
+    #[arg(short,default_value_t=false)]
+    l: bool,
 
-    if let Err(e) = ls_utils::ls_(path,CONFIG) {
+}
+
+
+fn test_function(config:Options){
+    let path = Path::new("/home/yssfbhr/Desktop/test_folder");
+
+    if let Err(e) = ls_utils::ls_(path,config) {
         eprintln!("Error: {}", e);
         process::exit(1);
     }
 
     process::exit(0);
+}
+fn main() {
+
+    let mut conf : Options = Options {
+        all: false,
+        long_format: false,
+    };
+
+    let args = Args::parse();
+    if args.all{
+        conf.all = true;
+        println!("all {}",args.all)
+    }
+    if args.l {
+        conf.long_format = true;
+        println!("long {}",args.l)
+
+    }
+
+    println!("{}  {}",conf.all,conf.long_format);
+    
+    test_function(conf);
 }
